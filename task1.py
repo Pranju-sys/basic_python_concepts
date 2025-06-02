3be68e0dde63ed1b71532ee0d838515f1d83058d
Pranju-sys <pranjalshaha8@gmail.com>
2025-06-02 19:48:59 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,10 @@
+a=int(input("enter the first number: "))
+b=int(input("enter the second number: "))
+c=a+b
+print("addition of numbers is:\t",c)
+c=a-b
+print("subtraction of number is:\t",c)
+c=a*b
+print("multiplication of numbers is:\t",c)
+c=a/b
+print("divison of numbers is:\t",c)
---
+++ task2.py
@@ -0,0 +1,4 @@
+str1=str(input("enter the first name\t"))
+str2=str(input("enter the second name\t"))
+c= f"{str1} {str2}"
+print(f"good morning! {c} have a wonderful day..")
